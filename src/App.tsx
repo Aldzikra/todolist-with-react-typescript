d2293dfc471fd9cc3691f200e3dea30d472fa579
Aldzikra <aldzikrarahmatdirizky@gmail.com>
2026-04-22 11:39:45 +0700

    feat: Tambah task ke dalam list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-function App(){
-  return(
-    <div className="h1">
-        <h1>My Planning App</h1>
+import { useState } from "react";
 
+function App(){
+  const [plans, setPlans] = useState<string[]> ([
+  "Belajar React",
+  "Kerjain Tugas",
+  "Rapat UKM"
+]);
+  const [input, setInput] = useState("");
+
+  const addTasks = () => {
+    if (input.trim() == "") return;
+
+    setPlans([...plans, input]);
+    setInput("")
+  }
+  return(
+    <div className="container">
+      <h1>My Planning App</h1>
+      <div className="input-group">
+        <input
+          type="text"
+          placeholder="Masukkan Rencanamu ...."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTasks}>Tambah</button>
+      </div>
+      
+      <ul>
+        {plans.map((plan, index) => (
+          <li key={index}>{plan}</li>
+        ))}
+      </ul>
     </div>
   )
 }
